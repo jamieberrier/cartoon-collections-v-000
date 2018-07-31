d8d7eed1259e1454006a12cf5a0ebf35760127e7
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 16:26:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,8 +15,9 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.include?(calls.length >= 4)
-
+  calls.each_with_index do |name. index|
+    calls.include?(name.length >= 4)
+  end
 end
 
 def find_the_cheese# code an argument here
