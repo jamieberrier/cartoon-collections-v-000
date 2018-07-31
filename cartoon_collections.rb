b8e00155b9845714dc4f2cdfd64d599dcab17def
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 16:07:42 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+
+  dwarves.each_with_index do |name|
+
+  end
+  
 end
 
 def summon_captain_planet# code an argument here
