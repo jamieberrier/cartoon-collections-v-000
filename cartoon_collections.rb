8976ea0ec36fd38aa943ba30204f39613b596b6d
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 16:06:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
+  # Your code here
+  names.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
+#  end
+end
+
+def summon_captain_planet(planeteer_calls)# code an argument here
+  # Your code here
+  planeteer_calls.map{ |planeteer| planeteer.capitalize }
+
+  planeteer_calls.each do |name|
+    name << !
+  end
+
+end
+
+def long_planeteer_calls(words)# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  #ingredients.include()
 end
