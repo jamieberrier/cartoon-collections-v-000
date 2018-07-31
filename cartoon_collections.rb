5167ec1084b2e2e6fa4726315d1738601c7ed282
jamieberrier <berrier.jamie@gmail.com>
2018-07-31 16:27:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 
 def long_planeteer_calls(calls)# code an argument here
   # Your code here
-  calls.each_with_index do |name, index|
+  calls.each do |name|
     calls.include?(name.length >= 4)
   end
 end
